f0e87518e25485e8f66a1edba128ea70eca9c4f8
PeonyF <1621139@gmail.com>
2020-02-09 01:05:03 +0900

    update to make the functions for SRP.

--- src/sort/InsertionSort.java
+++ src/sort/InsertionSort.java
@@ -5,38 +5,45 @@ class InsertionSort {
     int size;
     int j, temp;
 
-    InsertionSort(int[] insertArr, int size){
+    InsertionSort(int[] insertArr, int size) {
         this.insertArr = insertArr;
         this.size = size;
     }
 
-    void insertionSort(){
-        for(int i=1; i<size; i++){
+    void insertionSort() {
+        for (int i = 1; i < size; i++) {
             temp = insertArr[i];
             j = i;
-            while (biggerThanZeroOrTemp()){
-                insertArr[j] = insertArr[j-1];
-                j--;
-            }
+
+            swapInsertionSort();
+
             insertArr[j] = temp;
-            System.out.printf("삽입정렬 %d 단계 : ",i);
-            for(int t =0; t<size; t++){
+            System.out.printf("삽입정렬 %d 단계 : ", i);
+            for (int t = 0; t < size; t++) {
                 System.out.printf("%3d", insertArr[t]);
             }
             System.out.println();
         }
     }
 
-    private boolean biggerThanZeroOrTemp(){
-        return j>0 && temp<insertArr[j-1];
+    private void swapInsertionSort() {
+        while (biggerThanZeroOrTemp()) {
+            insertArr[j] = insertArr[j - 1];
+            j--;
+        }
+    }
+
+
+    private boolean biggerThanZeroOrTemp() {
+        return j > 0 && temp < insertArr[j - 1];
     }
 }
 
-class Main{
-    public static void main(String[] args){
-        int[] insertArr = {69,10,30,2,16,8,31,22};
+class Main {
+    public static void main(String[] args) {
+        int[] insertArr = {69, 10, 30, 2, 16, 8, 31, 22};
         int size = insertArr.length;
-        InsertionSort insertionSort = new InsertionSort(insertArr,size);
+        InsertionSort insertionSort = new InsertionSort(insertArr, size);
         System.out.print("\n정렬할 원소 : ");
         for (int value : insertArr) {
             System.out.printf(" %d", value);
